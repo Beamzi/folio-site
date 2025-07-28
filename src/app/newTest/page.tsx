5261db3ea68946fcc8e343b813de69342708da9d
James Day <159420270+Beamzi@users.noreply.github.com>
2025-07-28 23:00:31 +1000

    cube now as has X axis indication of interaction as as scrolling marquee relative to mouseX movement

--- src/app/newTest/page.tsx
+++ src/app/newTest/page.tsx
@@ -1,16 +1,20 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import {
+  easeInOut,
   motion,
+  useMotionTemplate,
   useMotionValue,
   useMotionValueEvent,
   useScroll,
   useTransform,
 } from "motion/react";
 import CubeFace from "@/components/CubeFace";
+import { LiaTabletSolid } from "react-icons/lia";
+import { HiOutlineMagnifyingGlassCircle } from "react-icons/hi2";
 
-const uniqueVales = [
+const faces = [
   "translate-z-12 rotate-x-0",
   "-translate-z-12",
   "translate-x-12 rotate-y-90",
@@ -21,46 +25,169 @@ const uniqueVales = [
 
 export default function page() {
   const wheelScale = useMotionValue(0.5);
-
   const mouseX = useMotionValue(45);
   const mouseY = useMotionValue(45);
+  const wheelUncap = useMotionValue(0.5);
 
+  useMotionValueEvent(wheelUncap, "change", (latest) => {
+    console.log("wheelscale changed to", latest);
+  });
+
+  const [switchFace, setSwitchFace] = useState(true);
+  const [faceIndex, setFaceIndex] = useState(10);
+  const [isAnimating, setIsAnimating] = useState(true);
+  const staticValues = [
+    [349, 393],
+    [350, 350],
+    [340, 240],
+    [380, 460],
+    [241, 340],
+    [480, 380],
+  ];
+
+  const template = useMotionTemplate`${wheelUncap}% 20%`;
+  const mouseXMulti = useTransform(mouseX, (value) => Math.ceil(value / 4));
+
+  const mouseXMultiColor = useTransform(mouseX, (value) =>
+    Math.max(50, Math.min(250, value / 4))
+  );
+
+  const marqueeColor = useMotionTemplate`rgb(${mouseXMultiColor}, 2, 255)`;
+
+  const marquee = useMotionTemplate`-${mouseXMulti}%`;
+
+  const setletterSpacer = () => {
+    const transform = useTransform(mouseX, (value) => Math.max(10, value / 20));
+    const finalTransform = !isAnimating ? transform : mouseX.get();
+    return useMotionTemplate`${finalTransform}px`;
+  };
+  const letterSpacer = setletterSpacer();
+
+  const setWebkitStroke = () => {
+    const transform = useTransform(mouseX, (value) =>
+      Math.min(20, Math.max(10, value / 20))
+    );
+    const finalTransform = !isAnimating ? transform : mouseX.get();
+    return useMotionTemplate`${finalTransform}px`;
+  };
+  const webkitStroke = setWebkitStroke();
+
+  useMotionValueEvent(webkitStroke, "change", (latest) => {
+    console.log("x changed to", latest);
+  });
+
+  const getLettersForMovement = (letter: string) => {
+    const letters = Array(1000).fill(letter);
+    const spans = [];
+    for (let i = 0; i < 2; i++) {
+      spans[i] = (
+        <motion.span
+          key={i + 10}
+          className=""
+          style={{
+            letterSpacing: letterSpacer,
+            WebkitTextStroke: webkitStroke,
+          }}
+        >
+          {letters}
+        </motion.span>
+      );
+    }
+    return <>{spans}</>;
+  };
   return (
     <main>
-      <section className="h-screen  flex justify-center items-center">
-        <motion.div
-          onMouseMove={(e) => {
-            mouseY.set(e.clientX);
-            mouseX.set(e.clientY);
-          }}
-          onWheel={(e) => {
-            const current = wheelScale.get();
-            const delta = e.deltaY / 2000;
-            wheelScale.set(Math.max(0.2, Math.min(2, current + delta)));
-          }}
-          style={{ rotateX: mouseX, rotateY: mouseY, scale: wheelScale }}
-          className="relative  scale-400  size-24 transform-3d rotate-x-45 rotate-y-45 ..."
-        >
-          {uniqueVales.map((item) => (
-            <CubeFace
-              mouseY={mouseY}
-              mouseX={mouseX}
-              key={item}
-              translateRotate={item}
-              className="absolute inset-0 border-1"
-            ></CubeFace>
+      <section className="h-screen relative flex justify-center items-center">
+        <div className="absolute left-0 flex   flex-col ">
+          {staticValues.map((item, index) => (
+            <button
+              onClick={() => {
+                // setSwitchFace(false);
+                setFaceIndex(index);
+                mouseX.set(item[0]);
+                mouseY.set(item[1]);
+                setIsAnimating(false);
+              }}
+              onMouseEnter={() => {
+                setFaceIndex(index);
+              }}
+              onMouseLeave={() => setFaceIndex(10)}
+              key={index}
+              className="face-1 hover:-mr-4 px-2 hover:bg-red-600 transition-all duration-100 py-1 border-1"
+            >{`face-${index}`}</button>
           ))}
-        </motion.div>
+        </div>
+        {/* sand-box-gradient */}
+
+        <section className="overflow-hidden  whitespace-nowrap w-200">
+          <section className="h-full w-full flex border-1 bg-black relative !z-100 justify-center flex-col items-center">
+            <h3 className="tracking-widest border-1 w-200 text-center">
+              A Portfolio By James Day
+            </h3>
+            <motion.p
+              animate={{ x: [-200, 0] }}
+              className="tracking-normal relative !z-100 text-neutral-600"
+            >
+              move your mouse around the space below to change the state of
+              things
+            </motion.p>
+          </section>
+
+          <motion.div
+            style={{ x: marquee, color: marqueeColor }}
+            animate={{ x: [0, "-50%"] }}
+            transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
+            className="w-full inline-flex relative -z-10  "
+          >
+            {getLettersForMovement("I")}
+          </motion.div>
+
+          <motion.div
+            style={{
+              backgroundPosition: template,
+            }}
+            className="radial-background w-full h-110 border-1 -mt-3.5  flex justify-center items-center"
+            onMouseMove={(e) => {
+              mouseY.set(e.clientY);
+              mouseX.set(e.clientX);
+            }}
+            onMouseEnter={() => {
+              setIsAnimating(false);
+            }}
+            onMouseLeave={() => {
+              setIsAnimating(true);
+            }}
+          >
+            <motion.div
+              onWheel={(e) => {
+                const current = wheelScale.get();
+                const delta = e.deltaY / 2000;
+                wheelScale.set(Math.max(0.2, Math.min(2, current + delta)));
+
+                wheelUncap.set(e.deltaY);
+              }}
+              style={{ rotateX: mouseX, rotateY: mouseY, scale: wheelScale }}
+              className={`relative scale-200 ${
+                isAnimating ? "init-animation" : "scale-400"
+              }   size-20 transform-3d rotate-x-45 rotate-y-45 `}
+            >
+              {faces.map((item, index) => (
+                <CubeFace
+                  setSwitchFace={setSwitchFace}
+                  switchFace={switchFace}
+                  faceIndex={faceIndex}
+                  index={index}
+                  mouseY={mouseY}
+                  mouseX={mouseX}
+                  key={item}
+                  translateRotate={item}
+                  className="absolute inset-0 border-2 "
+                ></CubeFace>
+              ))}
+            </motion.div>
+          </motion.div>
+        </section>
       </section>
     </main>
   );
-}
-
-{
-  /* <div className="absolute inset-0 translate-z-12 rotate-x-0 border-1 ..."></div>
-          <div className="absolute inset-0 -translate-z-12 border-1 ..."></div>
-          <div className="absolute inset-0 translate-x-12 rotate-y-90 border-1 ..."></div>
-          <div className="absolute inset-0 -translate-x-12 -rotate-y-90 border-1 ..."></div>
-          <div className="absolute inset-0 -translate-y-12 rotate-x-90 border-1 ..."></div>
-          <div className="absolute inset-0 translate-y-12 -rotate-x-90 border-1 ..."></div> */
 }
--- src/components/CubeFace.tsx
+++ src/components/CubeFace.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import { MotionValue } from "motion";
-import { useMotionValue } from "motion/react";
-import React, { Dispatch, SetStateAction } from "react";
+import {
+  useMotionValue,
+  useMotionValueEvent,
+  useTransform,
+} from "motion/react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { motion } from "motion/react";
+import { transferableAbortSignal } from "util";
 
 interface Props {
   translateRotate: string;
   className: string;
   mouseY: MotionValue<number>;
   mouseX: MotionValue<number>;
+  setSwitchFace: (value: boolean) => void;
+  switchFace: boolean;
+  faceIndex: number;
+  index: number;
   style?: {
     width: number;
     height: number;
@@ -17,18 +26,37 @@ interface Props {
 }
 
 export default function CubeFace({
+  faceIndex,
   translateRotate,
   className,
   mouseY,
   mouseX,
+  switchFace,
+  index,
 }: Props) {
+  const roundedX = useTransform(mouseX, [300, 400], [0, 100]);
+  const roundedY = useTransform(mouseY, [120, 500], [0, 100]);
+
   return (
-    <motion.div
-      onClick={() => {
-        mouseX.set(45);
-        mouseY.set(45);
-      }}
-      className={`${className} hover:bg-amber-800 transition-all duration-800 hover:perspective-distant boxing-shadow hover:-m-5 ${translateRotate}`}
-    ></motion.div>
+    <>
+      {
+        <motion.div
+          whileHover={{
+            margin: 2,
+            // backgroundColor: "red",
+            transition: { duration: 0.2 },
+          }}
+          style={{ borderRadius: roundedY }}
+          className={`${className} hover:perspective-distant outer-face-shadow content-center align-middle bg-neutral-500/30  z-10 ${
+            index > 1 && ""
+          } ${translateRotate} ${
+            faceIndex === index &&
+            "bg-emerald-300 transition-all duration-100 scale-120 face-hover "
+          }`}
+        >
+          {faceIndex === index && `face-${index}`}
+        </motion.div>
+      }
+    </>
   );
 }
